a755d22c0ae1915098862a051f29fd6b1f6880b3
Lois MD <ecirblo@hotmail.com>
2021-03-12 22:58:35 +0100

    changes from Brett

--- test_multi_scene_utils.js
+++ test_multi_scene_utils.js
@@ -1,33 +1,48 @@
+class MouseHandler {
+	
+	onMouseLeave(scene, engine, canvasName) {
+		console.log("leave " + canvasName);
+        scene.cameras[0].detachControl(document.getElementById(canvasName));
+	}
+	
+	onMouseEnter(scene, engine, canvasName) {
+		console.log("enter " + canvasName);
+		engine.inputElement = document.getElementById(canvasName);
+		document.getElementById(canvasName).blur();
+        scene.cameras[0].attachControl(document.getElementById(canvasName), true);
+	}
+}
+
 var createScenes = function (canvasesId) {
     var scenes = []
     for(let i = 0; i < canvasesId.length; i++){
         console.log(canvasesId[i]);
         // This creates a basic Babylon Scene object (non-mesh)
-        var scene = new BABYLON.Scene(engine);
+        let scene = new BABYLON.Scene(engine);
         
         // This creates and positions a free camera (non-mesh)
-        var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0.8, 5, new BABYLON.Vector3.Zero(), scene);
+        let camera = new BABYLON.ArcRotateCamera("Camera", 0, 0.8, 5, new BABYLON.Vector3.Zero(), scene);
         camera.setTarget(BABYLON.Vector3.Zero());
         
         camera.lowerRadiusLimit = 4;
         camera.upperRadiusLimit = 20;
         
         // This attaches the camera to the canvas
-        camera.attachControl(document.getElementById(canvasesId[i]), true);
+        //camera.attachControl(document.getElementById(canvasesId[i]), true);
         
         // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+        let light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
         
         // Default intensity is 1. Let's dim the light a small amount
         light.intensity = (i+1)*0.2;
         
         // Our built-in 'sphere' shape.
-        var box = BABYLON.MeshBuilder.CreateBox("Box", {size: 2}, scene);
+        let box = BABYLON.MeshBuilder.CreateBox("Box", {size: 2}, scene);
         
         box.position.y = 0.5;
         
-        var mat = new BABYLON.PBRMetallicRoughnessMaterial("mat", scene);
-        mat.baseColor = new BABYLON.Color3(0, (i+1)*0.2, 0);
+        let mat = new BABYLON.PBRMetallicRoughnessMaterial("mat", scene);
+        mat.baseColor = BABYLON.Color3.Random();
         mat.metallic = 1;
         mat.roughness = 0.5;
         
@@ -36,9 +51,12 @@ var createScenes = function (canvasesId) {
         //scene.createDefaultEnvironment();
         engine.registerView(document.getElementById(canvasesId[i]),camera);
         scenes.push(scene);
+		
+		let mouseHandler = new MouseHandler();
+		document.getElementById(canvasesId[i]).addEventListener("mouseleave", () => {mouseHandler.onMouseLeave(scene, engine, canvasesId[i]);}, false);
+		document.getElementById(canvasesId[i]).addEventListener("mouseenter", () => {mouseHandler.onMouseEnter(scene, engine, canvasesId[i]);}, false);
     }
 
     return scenes;
 
-};;
-
+};
